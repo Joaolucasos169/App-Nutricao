d0568d5c7b65b6202e00b95405a27eefd60c8c45
Breno <brbreno81@gmail.com>
2025-06-23 17:13:49 -0300

    Corrigida tela Home, criado NotificationsScreen e ajustado fluxo de cadastro

---
+++ mobile/navigation/AppNavigator.jsx
@@ -0,0 +1,30 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import WelcomeScreen from '../screens/Onboarding/WelcomeScreen';
+import LoginScreen from '../screens/Auth/LoginScreen';
+import RegisterScreen from '../screens/Auth/RegisterScreen';
+import ForgotPasswordScreen from '../screens/Auth/ForgotPasswordScreen';
+import HomeScreen from "../screens/Auth/HomeScreen";
+import NotificationsScreen from "../screens/Auth/NotificationsScreen";
+
+
+const Stack = createNativeStackNavigator();
+
+export default function AppNavigator() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator 
+        initialRouteName="Welcome"
+        screenOptions={{ 
+          headerShown: false,
+          animation: 'fade' // Adicione uma animação suave
+        }}
+      >
+        <Stack.Screen name="Welcome" component={WelcomeScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Register" component={RegisterScreen} />
+        <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
